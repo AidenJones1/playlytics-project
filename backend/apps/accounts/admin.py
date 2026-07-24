bc9e73f588a24d9ea11359bb123f3a768e049875
Aiden J. Jones <aidenjjones03@gmail.com>
2026-07-24 14:50:09 -0500

    Implement UserAdmin for User model in admin panel with enhanced display and filtering options

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.core.admin import playlytics_admin_site
+from apps.accounts.models import User
+
+@admin.register(User, site=playlytics_admin_site)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "favorite_team", "is_active", "is_superuser", "is_staff")
+    ordering = ("username",)
+
+    search_fields = ("username", "email")
+    list_filter = ("is_active", "is_superuser", "is_staff")
+
+    readonly_fields = ("id", "email", "password")
+    fieldsets = (
+        ("User Information", {
+            "fields": ("username", "email", "password", "favorite_team")}),
+        ("Permissions", {
+            "fields": ("is_active", "is_superuser", "is_staff")}),
+    )
