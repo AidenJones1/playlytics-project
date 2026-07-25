dfae6a180d85a3c01df93f42eab4bc7bbb724f70
Aiden J. Jones <aidenjjones03@gmail.com>
2026-07-25 09:06:15 -0500

    Add TeamSerializer for team model to handle team data representation

---
+++ backend/apps/teams/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from apps.teams.models import Team
+
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = [
+            'abbreviation',
+            'logo',
+            'color_1',
+            'color_2',
+            'color_3',
+            'color_4',
+        ]
