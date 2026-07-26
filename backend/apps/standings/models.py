a10a7b0224b78bb1168b6bf71755e3f5be450f2d
Aiden J. Jones <aidenjjones03@gmail.com>
2026-07-26 13:06:26 -0500

    Refactor TeamStandings model and admin to remove percentage and point differential fields, add points_for and points_against fields, and update ordering logic.

--- backend/apps/standings/models.py
+++ backend/apps/standings/models.py
@@ -13,7 +13,6 @@ class TeamStandings(models.Model):
     wins = models.PositiveIntegerField(default=0)
     losses = models.PositiveIntegerField(default=0)
     ties = models.PositiveIntegerField(default=0)
-    percentage = models.FloatField(default=0.0)
     home_wins = models.PositiveIntegerField(default=0)
     home_losses = models.PositiveIntegerField(default=0)
     home_ties = models.PositiveIntegerField(default=0)
@@ -26,15 +25,30 @@ class TeamStandings(models.Model):
     division_wins = models.PositiveIntegerField(default=0)
     division_losses = models.PositiveIntegerField(default=0)
     division_ties = models.PositiveIntegerField(default=0)
-    point_differential = models.IntegerField(default=0)
+    points_for = models.PositiveIntegerField(default=0)
+    points_against = models.PositiveIntegerField(default=0)
     streak = models.CharField(max_length=5, blank=False, null=False, default='-')
+
+    @property
+    def games_played(self):
+        return self.wins + self.losses + self.ties
+
+    @property
+    def percentage(self):
+        if self.games_played == 0:
+            return 0.0
+        return round((self.wins + (self.ties * 0.5)) / self.games_played, 3)
+
+    @property
+    def point_differential(self):
+        return self.points_for - self.points_against
 
     class Meta:
         unique_together = ('team', 'week')
         verbose_name = 'Team Standings'
         verbose_name_plural = 'Team Standings'
         db_table = 'team_standings'
-        ordering = ['-week__season__year', '-week__week', '-percentage', '-wins', 'losses', '-point_differential']
+        ordering = ['-week__season__year', '-week__week', '-wins', 'losses',]
 
     def __str__(self):
         return f"{self.team.fullname} - Week {self.week.week}"
