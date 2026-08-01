1380b20352466450bc978e53285436529141a041
Aiden J. Jones <aidenjjones03@gmail.com>
2026-08-01 17:50:51 -0500

    Add media file configuration to settings and update URL patterns for serving media files in development

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -32,3 +34,6 @@ urlpatterns = [
     path('api/models/', include('apps.models.urls', namespace='models')),
     path('api/dashboard/', include('apps.dashboard.urls', namespace='dashboard')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
